de51cebec8da26608f91add66db0b832ac03b624
muhammad usman <usmankhadim.uk.uk@gmail.com>
2024-11-02 01:43:06 +0500

    setup blog post model and add urls, views and paginator file

--- blog/models.py
+++ blog/models.py
@@ -1,23 +1,28 @@
-import pathlib
-import uuid
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
-
-
-def image_file_upload_handler(instance, filepath):
-    instance_id = instance.id
-    if not instance.id:
-        instance_id = "0"
-    filepath = pathlib.Path(filepath).resolve()
-    fname = str(uuid.uuid1())
-    ext = filepath.suffix
-    return f"blog/{instance_id}/{fname}/{filepath.name}"
 
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField(blank=True, null=True)
-    image = models.ImageField(upload_to=image_file_upload_handler, blank=True, null=True)
-    # timestamp = models.DateTimeField(auto_now_add=True)
-    # slug = models.SlugField(max_length=120, unique=True)
+    image = models.ImageField(upload_to="blogs/", blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("blog:post-detail", kwargs={"id": self.id})
+
+
+# import pathlib
+# import uuid
+# def image_file_upload_handler(instance, filepath):
+#     instance_id = instance.id
+#     if not instance.id:
+#         instance_id = "0"
+#     filepath = pathlib.Path(filepath).resolve()
+#     fname = str(uuid.uuid1())
+#     ext = filepath.suffix
+#     return f"blog/{instance_id}/{fname}/{filepath.name}"
